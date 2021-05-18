7f4773a533945baa9d871b2e300a1bde5b9a838f
AdrianSaut <adisautean@gmail.com>
2021-05-18 16:28:47 +0200

    Base de l'applicació

--- EscobarSnow/src/App.java
+++ EscobarSnow/src/App.java
@@ -1,21 +1,43 @@
+import java.io.FileInputStream;
+
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.Lighting;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
+
+// Cambieu només la part de la taula/table, lo altre mantenir per tenir-ho igual
 public class App extends Application{
-    Scene miEscena;
-    Pane container;
+    Scene miEscenaLloguer;
+    Pane containerLloguer;
     Text titol;
+    Image logo;
+    ImageView vistaLogo;
+    // TableView i TableColumn cambiar-ho amb la vostra apartat 
     TableView taula;
     TableColumn idCurs, curs, categoria, preu;
-    Button btnCursos, btnLlogar, btnCursosLlogats;
+
+    Button btnCursos, btnLlogar, btnCursosLlogats, btnSortir;
+    DropShadow sombra;
+    Lighting illuminacio;
     
     public static void main(String[] args) throws Exception {
 
@@ -28,10 +50,30 @@ public class App extends Application{
      
 
         // Nom de l'empresa / header
-        
-        
+        titol = new Text();
+        titol.setText("EscobarSnow");
+        titol.setTranslateX(150);
+        titol.setTranslateY(80);
+        // titol.setStyle(" -fx-text-inner-color: #FFFFFF");
+        titol.setFill(Color.web("#FFFFFF"));
 
-        // Taula
+        // Estils nom empresa
+        titol.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 50));
+
+        // Logotip de l'empresa
+        logo = new Image(new FileInputStream("C:/Users/DAM/Desktop/P6-AppEscriptori/EscobarSnow/src/Imatges/logo.png"));
+        vistaLogo = new ImageView(logo);
+
+        vistaLogo.setTranslateX(50);
+        vistaLogo.setTranslateY(20);
+
+        vistaLogo.setFitHeight(85);
+        vistaLogo.setFitWidth(85);
+
+        // Configuració de la proporció de conservació de la visualització de la imatge
+        vistaLogo.setPreserveRatio(true);
+
+        // Taula de lloguer *****Cambiar al vostre*****
         taula = new TableView();
         idCurs = new TableColumn<>("ID curs");
         curs = new TableColumn<>("Curs");
@@ -56,29 +98,66 @@ public class App extends Application{
         btnCursos.setTranslateX(800);
         btnCursos.setTranslateY(150);
 
+        
         btnLlogar = new Button("Llogar Cursos");
         btnLlogar.setMinWidth(100);
         btnLlogar.setTranslateX(800);
         btnLlogar.setTranslateY(200);
 
+        // Event del botó llogar
+        btnLlogar.setOnAction(e->containerLloguer.getChildren());
+
+
         btnCursosLlogats = new Button("Visualitzar cursos llogats");
         btnCursosLlogats.setMinWidth(100);
         btnCursosLlogats.setTranslateX(800);
         btnCursosLlogats.setTranslateY(250);
+
         
 
-        container = new Pane();
-        container.getChildren().addAll(taula, btnCursos, btnLlogar, btnCursosLlogats);
-        container.setMinSize(350, 250);
+        btnSortir = new Button("Sortir");
+        btnSortir.setMinWidth(100); 
+        btnSortir.setTranslateX(850);
+        btnSortir.setTranslateY(600);
 
-        miEscena = new Scene(container);
+        // Event del botó sortir
+        btnSortir.setOnAction(e->Platform.exit());
+        
+        estilsBotons();
+
+        containerLloguer = new Pane();
+        containerLloguer.getChildren().addAll(titol, vistaLogo, taula, btnCursos, btnLlogar, btnCursosLlogats, btnSortir);
+        containerLloguer.setMinSize(350, 250);
+        containerLloguer.setStyle(" -fx-base: #152348");
+
+        miEscenaLloguer = new Scene(containerLloguer);
 
         miEscenario.setTitle("Llogar cursos");
         miEscenario.setMinHeight(700);
         miEscenario.setMinWidth(1000);
 
-        miEscenario.setScene(miEscena);
+        miEscenario.setScene(miEscenaLloguer);
 
         miEscenario.show();
     }
+
+    public void estilsBotons(){
+        sombra = new DropShadow();
+        illuminacio = new Lighting();
+
+        btnCursos.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> btnCursos.setEffect(sombra));
+        btnCursos.addEventHandler(MouseEvent.MOUSE_EXITED, e -> btnCursos.setEffect(null));
+
+        btnLlogar.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> btnLlogar.setEffect(sombra));
+        btnLlogar.addEventHandler(MouseEvent.MOUSE_EXITED, e -> btnLlogar.setEffect(null));
+
+        btnCursosLlogats.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> btnCursosLlogats.setEffect(sombra));
+        btnCursosLlogats.addEventHandler(MouseEvent.MOUSE_EXITED, e -> btnCursosLlogats.setEffect(null));
+
+        btnSortir.addEventHandler(MouseEvent.MOUSE_ENTERED, e -> btnSortir.setEffect(illuminacio));
+        btnSortir.addEventHandler(MouseEvent.MOUSE_EXITED, e -> btnSortir.setEffect(null));
+        btnSortir.setStyle(" -fx-base: #FF0E0E");
+        
+    }
+
 }
